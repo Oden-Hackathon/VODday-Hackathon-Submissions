d97deb80a12606bd462fe7f3c0866f2a4a5282c9
Yudhvir Raj <rajyudhvir29@gmail.com>
2018-03-03 12:48:57 -0800

    Added maps

--- codebase/Safety-iOS/Safety/FirstViewController.swift
+++ codebase/Safety-iOS/Safety/FirstViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var MView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
