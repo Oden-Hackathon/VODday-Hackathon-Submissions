ca46593a4e7ccab4bf7417d951137320fa03992d
Brayden Traas <btraas@gmail.com>
2018-03-03 14:45:16 -0800

    list of alerts

---
+++ codebase/Safety-iOS/Safety/FeedAlertViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  FeedAlertViewCell.swift
+//  Safety
+//
+//  Created by Brayden Traas on 2018-03-03.
+//  Copyright © 2018 D'Arcy Smith. All rights reserved.
+//
+
+import UIKit
+
+class FeedAlertViewCell: UITableViewCell {
+
+  
+    @IBOutlet var alertTitleLabel: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
