10f8e25fbdcf8014bd362a42f54f062b4929fc10
D'Arcy Smith <ds@terratap.com>
2018-03-03 11:09:25 -0800

    inital commit

---
+++ codebase/Safety-iOS/Safety/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Safety
+//
+//  Created by D'Arcy Smith on 2018-03-03.
+//  Copyright © 2018 D'Arcy Smith. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
